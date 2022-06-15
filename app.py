dcf76258af9c130ab2e0acc24423cce375482cf3
WooSeock Hong <85264446+ikohong88@users.noreply.github.com>
2022-06-15 11:32:19 +0900

    2022-06-15 update
    - 보안을 위한 DB접근 코드 삭제

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.7rmjp.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient('보안을 위해 비공개')
 db = client.dbsparta
 
 @app.route('/')
